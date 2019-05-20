580f1929a0d1bd26b16fa25d63b2a1f880bdeabd
Stefan Mesken <kappadistributive@gmail.com>
2019-05-20 15:50:26 +0200

    fixed mathjax

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "This is a test",
-        "excerpt":"$e = mc^2$  ","categories": [],
+        "excerpt":"$e = mc^2$   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/test/",
         "teaser":null}]
