c98f44eda3eac0a13da7fb9fed544a47ae82316a
Stefan Mesken <kappadistributive@gmail.com>
2019-05-26 01:15:40 +0200

    fixed blog posts by tags

--- _pages/blog.md
+++ _pages/blog.md
@@ -5,21 +5,19 @@ title: "Blog Posts by Tags"
 author_profile: true
 ---
 
-{% assign rawtags = "" %}
-{% for post in site.posts %}
-	{% assign ttags = post.tags | join:'|' | append:'|' %}
-	{% assign rawtags = rawtags | append:ttags %}
-{% endfor %}
-{% assign rawtags = rawtags | split:'|' | sort %}
+{% for tag in site.tags %}
+  {% assign t = tag | first %}
+  {% assign posts = tag | last %}
 
-{% assign tags = "" %}
-{% for tag in rawtags %}
-	{% if tag != "" %}
-		{% if tags == "" %}
-			{% assign tags = tag | split:'|' %}
-		{% endif %}
-		{% unless tags contains tag %}
-			{% assign tags = tags | join:'|' | append:'|' | append:tag | split:'|' %}
-		{% endunless %}
-	{% endif %}
+{{ t | downcase }}
+<ul>
+{% for post in posts %}
+  {% if post.tags contains t %}
+  <li>
+    <a href="{{ post.url }}">{{ post.title }}</a>
+    <span class="date">{{ post.date | date: "%B %-d, %Y"  }}</span>
+  </li>
+  {% endif %}
+{% endfor %}
+</ul>
 {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "How To Beat Kaggle (the Easy Way)",
-        "excerpt":"A few nights ago, I found myself tinkering with the Titanic dataset on Kaggle and couldn’t help but notice the number of people with a perfect score – many of whom have a single entry. So, I thought to myself: “Clearly, they must be cheating. But how do you cheat...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/how-to-beat-kaggle-(the-easy-way)/",
+        "excerpt":"A few nights ago, I found myself tinkering with the Titanic dataset on Kaggle and couldn’t help but notice the number of people with a perfect score – many of whom have a single entry. So, I thought to myself: “Clearly, they must be cheating. But how do you cheat...","categories": ["Machine Learning"],
+        "tags": ["machine learning","mathematics","graph theory"],
+        "url": "http://localhost:4000/machine%20learning/how-to-beat-kaggle-(the-easy-way)/",
         "teaser":null}]
