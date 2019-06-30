b66666ee2471b5c829841ebcf9334deb314d0107
Stefan Mesken <kappadistributive@gmail.com>
2019-06-30 05:26:01 +0000

    updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,11 +4,6 @@ var store = [{
         "tags": ["machine learning","mathematics","graph theory"],
         "url": "http://localhost:4000/machine%20learning/how-to-beat-kaggle-(the-easy-way)/",
         "teaser":null},{
-        "title": "TensorFlow Without Tears",
-        "excerpt":"Life is pretty crazy right now. Finishing a PhD, changing from set theory to applied data science, forming connections to other data scientists, learning as much about machine learning (both theory and practice) as I possibly can, finding a new apartment and preparing to move, dealing with German bureaucracy… This...","categories": ["Machine Learning"],
-        "tags": ["machine learning"],
-        "url": "http://localhost:4000/machine%20learning/tensorflow-without-tears/",
-        "teaser":null},{
         "title": "An Hommage to Feynman's Technique",
         "excerpt":"Fragile Knowledge I don’t know what’s the matter with people: they don’t learn by understanding; they learn by some other way - by rote, or something. Their knowledge is so fragile! – Richard P. Feynman One key to successfully mastering a new subject, I believe, is to connect it to...","categories": ["Machine Learning"],
         "tags": ["neural networks","machine learning","human learning"],
@@ -18,9 +13,4 @@ var store = [{
         "excerpt":"Full disclosure: I’m not a big fan of one-dimensional metrics when evaluating (binary) classifiers (e.g. a neural network that is supposed to distinguish pictures of cats and dogs). In my opinion, a confusion matrix is, in almost all instances, preferable for data scientists. However, that doesn’t mean there aren’t good...","categories": ["Data-Science"],
         "tags": ["data science","machine learning","statistics"],
         "url": "http://localhost:4000/data-science/metrics/",
-        "teaser":null},{
-        "title": "Expected Value",
-        "excerpt":"A key realization that I had early in life, although in much more primitive terms, is that all you can ever hope to affect is the expected value of your decisions. Certainty, no matter how much we crave for it, simply isn’t attainable in most real life situations. It’s also...","categories": ["Probability Theory"],
-        "tags": ["neural networks","machine learning","human learning"],
-        "url": "http://localhost:4000/probability%20theory/expected-value/",
         "teaser":null}]
