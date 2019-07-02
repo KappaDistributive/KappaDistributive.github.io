bd35b99f23a86e75e27ea2e1762e62db5ef0b433
Stefan Mesken <kappadistributive@gmail.com>
2019-07-02 04:29:15 +0000

    tf2 without tears

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,6 +4,11 @@ var store = [{
         "tags": ["machine learning","mathematics","graph theory"],
         "url": "http://localhost:4000/machine%20learning/how-to-beat-kaggle-(the-easy-way)/",
         "teaser":null},{
+        "title": "TensorFlow Without Tears",
+        "excerpt":"Life is pretty crazy right now. Finishing a PhD, changing from set theory to applied data science, forming connections to other data scientists, learning as much about machine learning (both theory and practice) as I possibly can, finding a new apartment and preparing to move, dealing with German bureaucracy… This...","categories": ["Machine Learning"],
+        "tags": ["machine learning"],
+        "url": "http://localhost:4000/machine%20learning/tensorflow-without-tears/",
+        "teaser":null},{
         "title": "An Hommage to Feynman's Technique",
         "excerpt":"Fragile Knowledge I don’t know what’s the matter with people: they don’t learn by understanding; they learn by some other way - by rote, or something. Their knowledge is so fragile! – Richard P. Feynman One key to successfully mastering a new subject, I believe, is to connect it to...","categories": ["Machine Learning"],
         "tags": ["neural networks","machine learning","human learning"],
