ecbf099ace5f952c0bfad86de51db9f35eaa385b
Stefan Mesken <kappadistributive@gmail.com>
2021-03-22 19:59:51 +0100

    update about page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "How To Beat Kaggle (the Easy Way)",
         "excerpt":"A few nights ago, I found myself tinkering with the Titanic data set on Kaggle and couldn’t help but notice the number of people with a perfect score – many of whom have a single entry. So, I thought to myself: “Clearly, they must be cheating. But how do you...","categories": ["Machine Learning"],
         "tags": ["machine learning","mathematics","graph theory"],
-        "url": "http://localhost:4000/machine%20learning/how-to-beat-kaggle-(the-easy-way)/",
+        "url": "/machine%20learning/how-to-beat-kaggle-(the-easy-way)/",
         "teaser":null},{
         "title": "TensorFlow Without Tears",
         "excerpt":"Life is pretty crazy right now. Finishing a PhD, changing from set theory to applied data science, forming connections to other data scientists, learning as much about machine learning (both theory and practice) as I possibly can, finding a new apartment and preparing to move, dealing with German bureaucracy… This...","categories": ["Machine Learning"],
         "tags": ["machine learning"],
-        "url": "http://localhost:4000/machine%20learning/tensorflow-without-tears/",
+        "url": "/machine%20learning/tensorflow-without-tears/",
         "teaser":null},{
         "title": "An Hommage to Feynman's Technique",
         "excerpt":"Fragile Knowledge I don’t know what’s the matter with people: they don’t learn by understanding; they learn by some other way - by rote, or something. Their knowledge is so fragile! – Richard P. Feynman One key to successfully mastering a new subject, I believe, is to connect it to...","categories": ["Machine Learning"],
         "tags": ["neural networks","machine learning","human learning"],
-        "url": "http://localhost:4000/machine%20learning/Feynman-technique/",
+        "url": "/machine%20learning/Feynman-technique/",
         "teaser":null},{
         "title": "The Dangers of Performance Metrics",
         "excerpt":"Full disclosure: I’m not a big fan of one-dimensional metrics when evaluating (binary) classifiers (e.g. a neural network that is supposed to distinguish pictures of cats and dogs). In my opinion, a confusion matrix is, in almost all instances, preferable for data scientists. However, that doesn’t mean there aren’t good...","categories": ["Data-Science"],
         "tags": ["data science","machine learning","statistics"],
-        "url": "http://localhost:4000/data-science/metrics/",
+        "url": "/data-science/metrics/",
         "teaser":null},{
         "title": "Machine Learning Golf",
         "excerpt":"For the last 5 years or so, I’ve been lurking on the Code Golf Stackexchange. Code Golf is the art of solving a given problem, under specified rules, with as little written code as possible. And if you have never encountered ‘golfed code’, I highly recommend taking a look. Not...","categories": ["Data-Science"],
         "tags": ["machine learning","neural networks","ml golf"],
-        "url": "http://localhost:4000/data-science/machine-learning-golf/",
+        "url": "/data-science/machine-learning-golf/",
         "teaser":null}]
